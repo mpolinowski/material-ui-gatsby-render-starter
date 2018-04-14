f9204cef016828dc44cb5b927e4644ecf4a7032f
Mike Polinowski <mpolinowski@users.noreply.github.com>
2018-04-14 17:30:59 +0800

    initial commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import AppNav from '../components/app-nav';
+
+class Index extends React.Component {
+
+  render() {
+
+    return (
+      <React.Fragment>
+        <AppNav />
+      </React.Fragment>
+    );
+  }
+}
+
+export default Index;
